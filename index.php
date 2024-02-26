7bb3fd0882544510eeaf518266db3400fb77c686
Lorenzo7991 <lorenzo1997chierisini@gmail.com>
2024-02-26 16:56:21 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" href="assets\favicon.ico">
+    <title>(OOP 2)e-shop</title>
+    <!-- MyCSS -->
+    <link rel="stylesheet" href="css\style.css">
+</head>
+
+<body>
+    <h1>Test...</h1>
+</body>
+
+</html>
