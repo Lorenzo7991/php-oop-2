64aa6b5bd6f26e15237eacd61540e6136408a2af
Lorenzo7991 <lorenzo1997chierisini@gmail.com>
2024-02-26 18:10:59 +0100

    restructured provided data

---
+++ config/data-products.php
@@ -0,0 +1,68 @@
+<?php
+$dataProducts = [
+    [
+        "id" => 1,
+        "name" => "Royal Canin Mini Adult",
+        "description" => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque saepe blanditiis libero soluta natus nobis in hic dolorum exercitationem ducimus eveniet a architecto provident excepturi dolor quidem, quas illo cumque!",
+        "price" => "39.90",
+        "category" => "Cane",
+        "images" => "https://arcaplanet.vtexassets.com/arquivos/ids/284621/Mini-Adult.jpg?v=638182891693570000"
+    ],
+    [
+        "id" => 2,
+        "name" => "Almo Nature Holistic Maintenance Medium Large Tonno e Riso",
+        "description" => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque saepe blanditiis libero soluta natus nobis in hic dolorum exercitationem ducimus eveniet a architecto provident excepturi dolor quidem, quas illo cumque!",
+        "price" => "18.90",
+        "category" => "Gatto",
+        "images" => "https://arcaplanet.vtexassets.com/arquivos/ids/245173/almo-nature-holistic-cane-adult-medium-pollo-e-riso.jpg"
+    ],
+    [
+        "id" => 3,
+        "name" => "Almo Nature Cat Daily Lattina",
+        "description" => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque saepe blanditiis libero soluta natus nobis in hic dolorum exercitationem ducimus eveniet a architecto provident excepturi dolor quidem, quas illo cumque!",
+        "price" => "19.90",
+        "category" => "Gatto",
+        "images" => "https://arcaplanet.vtexassets.com/arquivos/ids/245336/almo-daily-menu-cat-400-gr-vitello.jpg"
+    ],
+    [
+        "id" => 4,
+        "name" => "Mangime per Pesci Guppy in Fiocchi",
+        "description" => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque saepe blanditiis libero soluta natus nobis in hic dolorum exercitationem ducimus eveniet a architecto provident excepturi dolor quidem, quas illo cumque!",
+        "price" => "29.90",
+        "category" => "Pesci",
+        "images" => "https://arcaplanet.vtexassets.com/arquivos/ids/272714/tetra-guppy-mini-flakes.jpg"
+    ],
+    [
+        "id" => 5,
+        "name" => "Voliera Wilma in Legno",
+        "description" => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque saepe blanditiis libero soluta natus nobis in hic dolorum exercitationem ducimus eveniet a architecto provident excepturi dolor quidem, quas illo cumque!",
+        "price" => "69.90",
+        "category" => "Uccelli",
+        "images" => "https://arcaplanet.vtexassets.com/arquivos/ids/258384/voliera-wilma1.jpg"
+    ],
+    [
+        "id" => 6,
+        "name" => "Cartucce Filtranti per Filtro EasyCrystal",
+        "description" => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque saepe blanditiis libero soluta natus nobis in hic dolorum exercitationem ducimus eveniet a architecto provident excepturi dolor quidem, quas illo cumque!",
+        "price" => "19.90",
+        "category" => "Pesci",
+        "images" => "https://arcaplanet.vtexassets.com/arquivos/ids/272741/tetra-easycrystal-filterpack-250-300.jpg"
+    ],
+    [
+        "id" => 7,
+        "name" => "Kong Classic",
+        "description" => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque saepe blanditiis libero soluta natus nobis in hic dolorum exercitationem ducimus eveniet a architecto provident excepturi dolor quidem, quas illo cumque!",
+        "price" => "9.90",
+        "category" => "Cane",
+        "images" => "https://arcaplanet.vtexassets.com/arquivos/ids/256599/kong-classic1.jpg"
+    ],
+    [
+        "id" => 8,
+        "name" => "Topini di peluche Trixie",
+        "description" => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque saepe blanditiis libero soluta natus nobis in hic dolorum exercitationem ducimus eveniet a architecto provident excepturi dolor quidem, quas illo cumque!",
+        "price" => "9.90",
+        "category" => "Gatto",
+        "images" => "https://arcaplanet.vtexassets.com/arquivos/ids/223852/trixie-gatto-gioco-active-mouse-peluche.jpg"
+    ]
+];
+?>
